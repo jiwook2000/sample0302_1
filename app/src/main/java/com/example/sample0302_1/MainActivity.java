4eac87dd2c44b4ec56a8f84dfa21cb3e11ce470d
jiwook2000 <56825960+jiwook2000@users.noreply.github.com>
2020-03-02 22:19:59 +0900

    done

--- app/src/main/java/com/example/sample0302_1/MainActivity.java
+++ app/src/main/java/com/example/sample0302_1/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Mylayout layout1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layout1 = findViewById(R.id.layout1);
+        layout1.setName("Chung");
+        layout1.setMobile("010-1234-5678");
+
     }
 }
